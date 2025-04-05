40fdff28010f23acef30a89e544205f50a3c8f72
SunoopDogg <aswoo55555@gmail.com>
2025-04-05 18:39:05 +0000

    refactor: Remove deprecated Bim page and route

--- app/bim/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Bim } from '@/src/2pages/bim';
-
-export default async function BimPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-
-  return <Bim id={id} />;
-}
